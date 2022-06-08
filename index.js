affec4035bec1c12211eeac516f9ce8f33b72efa
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-08 08:53:48 +0300

    commit tree 8c939520314ec0b51863dcee43eca2dd68024dee

---
+++ index.js
@@ -0,0 +1,23 @@
+
+	function introduction(name) {
+		if (name === "Aki")
+		return `Hi, my name is ${name}.`
+			else if (name === "Samip")
+			return `Hi, my name is ${name}.`
+		}
+
+
+	function introductionWithLanguage(name, language){
+		if (name,language === "Aki", "Ember.js")
+		return `Hi, my name is ${name} and I am learning to program in ${language}.`
+			else if (name, language === "Samip", "React")
+				return `Hi my name is ${name} and I am learning to program in ${language} `
+		
+				
+		}
+	function introductionWithLanguageOptional (name, language = "JavaScript"){
+		if (name === "Gracie")
+			return `Hi, my name is ${name} and I am learning to program in ${language}.`
+			else if (name,language === "Gracie", "Python")
+				return `Hi, my name is ${name} and I am learning to program in ${language}.`
+	}
